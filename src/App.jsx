1096c86b2a73806ed248a670c36e32399d130c7a
Akash Menon <akashmenon11699cool@hotmail.com>
2025-06-15 20:22:58 +0530

    Add photo, text and BG

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-// import HeroSection from "./sections/HeroSection";
-
 import AnimatedBackground from "./components/AnimatedBackground";
 
 function App() {
-  return (
-    <>
-    <AnimatedBackground />
-      {/* <HeroSection
-        background="linear-gradient(135deg, #ff8ae2, #8aa7ff)"
-        text="Every moment with you feels like a dream."
-        highlightWords={["dream"]}
-        image="/images/image1.jpeg"
-        caption="Captured in my heart forever."
-      />
-
-      <HeroSection
-        background="linear-gradient(135deg, #fcb045, #fd1d1d, #833ab4)"
-        text="Your smile lights up my entire world."
-        highlightWords={["smile", "world"]}
-        image="/images/image1.jpeg"
-        caption="Every glance is a spark."
-      /> */}
-    </>
-  );
+  return <AnimatedBackground />;
 }
 
 export default App;
--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -1,11 +1,16 @@
 import StarsBackground from "./StarsBackground";
-import "../styles/AnimatedBackground.css";
+import FancyText from "./FancyText";
 import PhotoCarousel from "./PhotoCarousel";
+import "../styles/AnimatedBackground.css";
 
 const AnimatedBackground = () => {
   return (
     <div className="animated-background">
       <StarsBackground />
+      <FancyText
+        text="This is to certify that Ridhi is the cutest girl in the universe"
+        highlightWords={["Ridhi", "cutest", "universe"]}
+      />
       <PhotoCarousel />
     </div>
   );
